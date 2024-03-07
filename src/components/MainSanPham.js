66295469eef4db76e258283b7371e04806970678
unknown <trantantu85@gmail.com>
2024-03-07 21:07:25 +0700

    2

--- src/components/MainSanPham.js
+++ src/components/MainSanPham.js
@@ -3,9 +3,33 @@ import '../css/sanpham.css';
 import ItemSanPham from "./ItemSanPham";
 
 export default function MainSanPham(){
+  function activeall(){ 
+    document.getElementById('allsp').classList.add('danhmuc-item-active')
+    document.getElementById('gom').classList.remove('danhmuc-item-active')
+    document.getElementById('say').classList.remove('danhmuc-item-active')
+    document.getElementById('wax').classList.remove('danhmuc-item-active')
+  }
+    function activewax(){ 
+      document.getElementById('wax').classList.add('danhmuc-item-active')
+      document.getElementById('gom').classList.remove('danhmuc-item-active')
+      document.getElementById('say').classList.remove('danhmuc-item-active')
+      document.getElementById('allsp').classList.remove('danhmuc-item-active')
+    }
+    function activegom(){
+      document.getElementById('gom').classList.add('danhmuc-item-active')
+      document.getElementById('wax').classList.remove('danhmuc-item-active')
+      document.getElementById('say').classList.remove('danhmuc-item-active') 
+      document.getElementById('allsp').classList.remove('danhmuc-item-active')
+    }
+    function activesay(){
+      document.getElementById('say').classList.add('danhmuc-item-active')
+      document.getElementById('gom').classList.remove('danhmuc-item-active')
+      document.getElementById('wax').classList.remove('danhmuc-item-active')
+      document.getElementById('allsp').classList.remove('danhmuc-item-active')
+    }
     return(
       <main>
-        <section id="collection-template">
+ <section id="collection-template">
   <div className="collection-body">
     <div className="app-container">
       <div className="grid">
@@ -14,18 +38,23 @@ export default function MainSanPham(){
             <i className="fas fa-list icondanhmuc" /> Danh Mục
           </h4>
           <ul className="danhmuc-list">
-            <li className="danhmuc-item danhmuc-item-active">
-              <a href="#" className="danhmuc-item-link">
+            <li className="danhmuc-item " >
+              <a onClick={activeall} id="allsp" href="#" className="danhmuc-item-link danhmuc-item-active">
+                Tất Cả Sản Phẩm
+              </a>
+            </li>
+            <li className="danhmuc-item " >
+              <a onClick={activewax} id="wax" href="#" className="danhmuc-item-link">
                 Sáp Vuốt Tóc
               </a>
             </li>
-            <li className="danhmuc-item">
-              <a href="#" className="danhmuc-item-link">
+            <li className="danhmuc-item" >
+              <a onClick={activegom} id="gom" href="#" className="danhmuc-item-link no-active">
                 Gôm Tạo Kiểu
               </a>
             </li>
-            <li className="danhmuc-item">
-              <a href="#" className="danhmuc-item-link">
+            <li className="danhmuc-item" >
+              <a onClick={activesay} id="say" href="#" className="danhmuc-item-link no-active">
                 Máy Sấy Tóc
               </a>
             </li>
